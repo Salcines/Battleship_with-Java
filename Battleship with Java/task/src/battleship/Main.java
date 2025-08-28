6d42c744587e5e695cdaf71c644df2aa9524cf6b
salcines <salcines@outlook.es>
2025-08-28 18:50:03 +0200

    Battlefield with Java. Print empty field

--- Battleship with Java/task/src/battleship/Main.java
+++ Battleship with Java/task/src/battleship/Main.java
@@ -2,6 +2,16 @@ package battleship;
 
 public class Main {
 
+    static final char FOG_OF_WAR = '~';
+    static final char CELL_WITH_SHIP = '0';
+    static final char HIT = 'X';
+    static final char MISS = 'M';
+
+    static final int FILES_FIELD = 10;
+    static final int COLUMNS_FIELD = 10;
+
+    static final int HEADER = 1;
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
