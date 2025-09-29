f667191aac96bf1b1dbaa7184b6e2123a8075540
salcines <salcines@outlook.es>
2025-09-29 08:32:24 +0200

    Floating-point types. Task 7 - Calculate area of a circle when a given radius

---
+++ Topics/Floating-point types and operations/Area of a circle/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int r = scanner.nextInt();
+        scanner.close();
+        double s = Math.PI * r * r;
+
+        System.out.println(s);
+
+    }
+}
