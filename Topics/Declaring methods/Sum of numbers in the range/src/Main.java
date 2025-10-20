16652a7de0be2bef974f59ad58b9e5d629ebf410
salcines <salcines@outlook.es>
2025-10-21 01:58:53 +0200

    Declaring methods. Task 4 - Sum the numbers in the range below to up - 1

---
+++ Topics/Declaring methods/Sum of numbers in the range/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * The method calculates the sum of integers in a given range
+     * 
+     * @param from inclusive
+     * @param to exclusive
+     *
+     * @return the sum (long)
+     */
+    public static long sumInRange(int from, int to) {
+
+        if (from == to) {
+            return 0;
+        }
+
+        //O(1)
+        int interval = to - from;
+
+        return ((from + (long) (to - 1)) * interval) / 2;
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+
+        int from = scanner.nextInt();
+        int to = scanner.nextInt();
+
+        System.out.println(sumInRange(from, to));
+    } 
+}
